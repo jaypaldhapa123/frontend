9ec4e345b3e2b96404d3c4e9380888700b7f17cc
jaypaldhapa123 <jaypaldhapa7990@gmail.com>
2026-01-29 21:09:42 +0530

    fix some bugs

--- form.js
+++ form.js
@@ -98,7 +98,7 @@ contactForm.addEventListener("submit", function (event) {
     // send data into backend
 
     //fetch("https://backend-production-95f99.up.railway.app/formdata", {
-    fetch("backend-production-b013.up.railway.app/formdata", {
+    fetch("https://backend-production-b013.up.railway.app/formdata", {
       method: "post",
       headers: { "content-Type": "application/json" },
       body: JSON.stringify(formdata),
